e4efa8add0c45c95bb093f5f6a89f62e9186baa4
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-12-19 13:08:33 -0700

    Added images and header.

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>
+        MINE GRINDER:
+        <br />A LEGACY OF HORROR
+      </h1>
+    </div>
+  );
+}
