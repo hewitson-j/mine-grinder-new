c88b6f05e3abc242e995aa83640edf776e9f9885
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-12-19 14:12:43 -0700

    Added mobile navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,30 @@
+import { useEffect, useState } from "react";
 import "./Header.css";
 import Navbar from "./Navbar";
+import MobileNavbar from "./MobileNavbar";
 
 export default function Header() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="header">
       <h1>
         MINE GRINDER:
         <br />A LEGACY OF HORROR
       </h1>
-      <Navbar />
+      {windowWidth >= 730 ? <Navbar /> : <MobileNavbar />}
     </div>
   );
 }
---
+++ src/components/MobileNavbar.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import "./MobileNavbar.css";
+
+export default function MobileNavbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="mobile-navbar">
+      <div className="mobile-navbar-buttons">
+        <button id="mobile-hamburger-button" onClick={handleOpen}>
+          ☰
+        </button>
+        <button id="mobile-prevent-animation-button">Prevent Animation</button>
+      </div>
+      {isOpen ? (
+        <div className="mobile-navbar-menu">
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about">About the Author</a>
+            </li>
+            <li>
+              <a href="#art">Art</a>
+            </li>
+            <li>
+              <a href="#buy">Buy Book</a>
+            </li>
+          </ul>
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+}
