463ab43fe6e681684f9b300ed1f7516037557d32
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-12-21 15:53:52 -0700

    Added paragraph breaks, fixed font size for smaller screens.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -17,15 +17,17 @@ export default function Home() {
           to the legacy of the 80’s horror movie genre that will unnerve you as
           you turn the pages. Horror fans will love the bloody misfortune into a
           forbidden mining town’s secret past, Abismo de la Plata, The Silver
-          Abyss as it was named. Surrounded by darkness, the friends only have
-          themselves to rely on as a mine collapse traps them and a robotic
-          executioner aims to slaughter them, one by one. Chased by the cold
-          machine that is as methodical as any serial killer in any story that
-          came before. It is a story of pain and graphic death that spirals into
-          the weaved web of a historically insane genius left from the mine’s
-          past. What secrets does the darkness of Abismo de la Plata hold and
-          can they find their way out before they become lost to the legend of
-          the Silver Abyss?
+          Abyss as it was named.
+          <br />
+          <br />
+          Surrounded by darkness, the friends only have themselves to rely on as
+          a mine collapse traps them and a robotic executioner aims to slaughter
+          them, one by one. Chased by the cold machine that is as methodical as
+          any serial killer in any story that came before. It is a story of pain
+          and graphic death that spirals into the weaved web of a historically
+          insane genius left from the mine’s past. What secrets does the
+          darkness of Abismo de la Plata hold and can they find their way out
+          before they become lost to the legend of the Silver Abyss?
         </p>
       </div>
     </div>
