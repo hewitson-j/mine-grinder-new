36504ace80f0d030f830173fd877b49f8f99319c
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-12-21 15:48:50 -0700

    Added about author section.

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,9 +1,11 @@
+import AboutAuthor from "./AboutAuthor";
 import Home from "./Home";
 
 export default function Body() {
   return (
     <>
       <Home />
+      <AboutAuthor />
     </>
   );
 }
