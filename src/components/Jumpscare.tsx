2e7840ffb8b53a3509cb228edc4d0ae3375a215d
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-12-24 13:43:25 -0700

    Added image to jumpscare

--- src/components/Jumpscare.tsx
+++ src/components/Jumpscare.tsx
@@ -1,5 +1,17 @@
 import "./Jumpscare.css";
+import mg1 from "../assets/MG1.jpg";
 
 export default function Jumpscare() {
-  return <div className="jumpscare">Hello...</div>;
+  return (
+    <div className="jumpscare">
+      <div className="jumpscare-body">
+        <img
+          src={mg1}
+          title="Beware the Mine Grinder"
+          alt="Beware the Mine Grinder"
+          id="grinder-image"
+        />
+      </div>
+    </div>
+  );
 }
