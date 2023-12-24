37a59bda361b678baa299cdf9e7ba0eb568a345e
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-12-24 14:10:13 -0700

    Added in animations.

--- src/components/Jumpscare.tsx
+++ src/components/Jumpscare.tsx
@@ -5,12 +5,16 @@ export default function Jumpscare() {
   return (
     <div className="jumpscare">
       <div className="jumpscare-body">
+        <div id="message1">RUN</div>
+        <div id="message2">FIGHT</div>
         <img
           src={mg1}
           title="Beware the Mine Grinder"
           alt="Beware the Mine Grinder"
           id="grinder-image"
         />
+        <div id="message3">HIDE</div>
+        <div id="message4">DIE</div>
       </div>
     </div>
   );
