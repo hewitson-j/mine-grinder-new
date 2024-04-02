7133755c110453385773e2f1d63cecb722538cec
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2024-04-02 10:22:11 -0600

    Added privacy policy component and route

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -3,6 +3,7 @@ import Content from "./Content";
 import Jumpscare from "./Jumpscare";
 import ErrorPage from "./ErrorPage";
 import TermsOfService from "./TermsOfService";
+import PrivacyPolicy from "./PrivacyPolicy";
 
 export default function Router() {
   return (
@@ -10,7 +11,8 @@ export default function Router() {
       <Route path="/" element={<Content />} />
       <Route path="it" element={<Jumpscare />} />
       <Route path="error" element={<ErrorPage />} />
-      <Route path="termsofservice" element={<TermsOfService/>} />
+      <Route path="termsofservice" element={<TermsOfService />} />
+      <Route path="privacypolicy" element={<PrivacyPolicy />}/>
       <Route path="*" element={<Navigate to={"/error"} replace />} />
     </Routes>
   );
