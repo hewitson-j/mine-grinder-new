2e4255608814b570e8754ce897374c195f9f4b67
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2024-09-16 10:52:12 -0600

    Moved to utils folder

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,5 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import Content from "./Content";
+import Content from "./utils/Content";
 import Jumpscare from "./Jumpscare";
 import ErrorPage from "./ErrorPage";
 import TermsOfService from "./TermsOfService";
@@ -12,7 +12,7 @@ export default function Router() {
       <Route path="it" element={<Jumpscare />} />
       <Route path="error" element={<ErrorPage />} />
       <Route path="termsofservice" element={<TermsOfService />} />
-      <Route path="privacypolicy" element={<PrivacyPolicy />}/>
+      <Route path="privacypolicy" element={<PrivacyPolicy />} />
       <Route path="*" element={<Navigate to={"/error"} replace />} />
     </Routes>
   );
--- src/components/utils/Body.tsx
+++ src/components/utils/Body.tsx
@@ -1,8 +1,8 @@
-import AboutAuthor from "./about/AboutAuthor";
-import Art from "./art/Art";
-import Buy from "./buy/Buy";
+import AboutAuthor from "../about/AboutAuthor";
+import Art from "../art/Art";
+import Buy from "../buy/Buy";
 import Copyright from "./Copyright";
-import Home from "./Home";
+import Home from "../Home";
 
 export default function Body() {
   return (
